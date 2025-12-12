d3f6c739c3e0685e30a72731722a838822b7a7be
t-endo <proof.endo@gmail.com>
2025-12-12 23:34:21 +0900

    Initial Commit

---
+++ test_gameTests/test_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_gameTests.swift
+//  test_gameTests
+//
+//  Created by 遠藤貴憲 on 2025/12/12.
+//
+
+import Testing
+@testable import test_game
+
+struct test_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
